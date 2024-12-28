fe47797020cc4fd2e518f3508049ef4edc1996f8
herin <herinsoni3737@gmail.com>
2024-12-28 16:08:02 +0530

    Final Setup

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <>
+            <h1>  404 Not Found</h1>
+            <h3>  The page You are looking for doesn't exist</h3>
+        </>
+    )
+}
+
+export default NotFound
